89d8f6ef2653c306a4a62266fcde5321734feaab
David Anderson <andersonda12@students.ecu.edu>
2018-05-24 16:22:32 -0400

    initial layout work on main activity for conducting debates

--- app/src/main/java/com/danderson/provemewrong/OverviewFragment.kt
+++ app/src/main/java/com/danderson/provemewrong/OverviewFragment.kt
@@ -6,25 +6,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
-import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import com.danderson.provemewrong.adapters.UserAdapter
-import com.danderson.provemewrong.debatemodel.DebateBase
-import com.danderson.provemewrong.debatemodel.TimedDebate
-import com.danderson.provemewrong.debatemodel.User
-import com.google.firebase.auth.FirebaseAuth
-import java.text.SimpleDateFormat
-import java.util.*
+import com.danderson.provemewrong.adapters.DebatesAdapter
 
-/**
- * A simple [Fragment] subclass.
- *
- */
 class OverviewFragment : Fragment() {
 
     private var layoutManager: RecyclerView.LayoutManager? = null
@@ -39,7 +27,7 @@ class OverviewFragment : Fragment() {
 
         layoutManager = LinearLayoutManager(context)
         debates.layoutManager = layoutManager
-        adapter = DebatesAdapter()
+        adapter = DebatesAdapter(context!!)
         debates.adapter = adapter
 
         fab.setOnClickListener{
@@ -47,46 +35,5 @@ class OverviewFragment : Fragment() {
         }
 
         return v
-    }
-
-    inner class DebatesAdapter: RecyclerView.Adapter<DebatesAdapter.ViewHolder>(){
-
-        var debates = DebateBase.getDebates(FirebaseAuth.getInstance().currentUser!!.uid, this as RecyclerView.Adapter<*>)
-
-        inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-            var topic: TextView = itemView.findViewById(R.id.card_topic)
-            var category: TextView = itemView.findViewById(R.id.card_category)
-            var participants: RecyclerView = itemView.findViewById(R.id.card_participants)
-            var time: TextView = itemView.findViewById(R.id.card_time)
-            var end: TextView = itemView.findViewById(R.id.card_expiration)
-
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val v = LayoutInflater.from(parent.context).inflate(R.layout.card_debate, parent, false)
-            return ViewHolder(v)
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val debate = debates[position]
-            holder.topic.text = debate.topic
-            holder.category.text = debate.category
-            val format = SimpleDateFormat("EEE, MMM d", Locale.US)
-            holder.time.text = String.format(getString(R.string.last_activity), format.format(Date()))
-            holder.end.text = if(debate is TimedDebate)
-                                String.format(getString(R.string.end_date), format.format(Date()))
-                              else
-                                String.format(getString(R.string.end_date), "none")
-
-            holder.participants.layoutManager = GridLayoutManager(activity, 4)
-
-            val adapter = UserAdapter<User>()
-            adapter.users = DebateBase.getParticipantsForDebate(debate.id!!, adapter)
-            holder.participants.adapter = adapter
-        }
-
-        override fun getItemCount(): Int {
-            return debates.size
-        }
     }
 }
