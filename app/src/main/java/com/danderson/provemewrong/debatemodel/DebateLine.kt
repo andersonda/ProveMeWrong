9abc455fe2b99f227c24a11508db6b2e8954c43f
David Anderson <andersonda12@students.ecu.edu>
2018-05-25 20:12:48 -0400

    added time field to the DebateLine model

--- app/src/main/java/com/danderson/provemewrong/debatemodel/DebateLine.kt
+++ app/src/main/java/com/danderson/provemewrong/debatemodel/DebateLine.kt
@@ -1,7 +1,6 @@
 package com.danderson.provemewrong.debatemodel
 
-class DebateLine(val user: String, val content: String) {
-
+class DebateLine(val user: User, val content: String, val time: String) {
     val likedBy = mutableListOf<String>()
     val references = mutableListOf<String>()
 }
